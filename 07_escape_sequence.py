1193cb3d093c3cf1abc6a1301de6cdd003ef7f86
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2024-08-29 13:08:00 +0530

    escape and print method

---
+++ 07_escape_sequence.py
@@ -0,0 +1,9 @@
+#comment 
+"""hey vinay you are alone,
+once you realise  """
+
+# this is right\n not /this
+print("hello vinay you completed your work \nhow much ")
+print("you know me")
+print("vinay",7,77, end="009") #use\n to change the line
+print("great")
